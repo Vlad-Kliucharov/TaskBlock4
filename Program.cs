2aaec4a58245f2ace30afd82a2aa741fa523bbe7
Vladyslav Kliucharov <vkliucharov@langate.com>
2021-04-08 18:11:49 +0300

    Comment added

--- Program.cs
+++ Program.cs
@@ -113,26 +113,26 @@ namespace TaskBlock2
                     break;
             }
         }
-/*        public static void SelectDepartment(List<PersonAttribute> PersonsList)
-        {
-            //List name  OrderBy(LINQ)           convert to list 
-            PersonsList.OrderBy(i => i.Department).ToList().ForEach(y => Console.WriteLine($"Departmnet : {y.Department}"));
+        /*        public static void SelectDepartment(List<PersonAttribute> PersonsList)
+                {
+                    //List name  OrderBy(LINQ)             convert to list for using foreach
+                    PersonsList.OrderBy(i => i.Department).ToList().ForEach(y => Console.WriteLine($"Departmnet : {y.Department}"));
 
-        }*/
+                }*/
 
         public static void SelectDepartment(List<PersonAttribute> PersonsList)
         {
-            //List name  OrderBy(LINQ)           convert to list 
             Console.WriteLine("Enter a department which you wanna find");
             var departmentName = Console.ReadLine();
+                                  //List name "Where" selection attribute(LINQ) shifting to lower for any search variants contains value in list  convert to list for using foreach
             var selecteDepartment = PersonsList.Where(attr => attr.Department.ToLower().Contains(departmentName.ToLower())).ToList();
+                              //Loop for selecting from list
             selecteDepartment.ForEach(department => Console.WriteLine($"Department : {department.Department}, Name : {department.Name}"));
             
         }
 
         public static void SelectUserByName(List<PersonAttribute> PersonsList)
         {
-            //List name  OrderBy(LINQ)           convert to list 
             Console.WriteLine("Enter a name for the user which you wanna find");
             var userName = Console.ReadLine();
             var selectedUsers = PersonsList.Where(attr => attr.Name.ToLower().Contains(userName.ToLower())).ToList();
